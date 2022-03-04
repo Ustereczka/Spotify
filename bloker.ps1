479b4e17b74ad849000f3c997f9bed67f935aa9c
Ustereczka <100975434+Ustereczka@users.noreply.github.com>
2022-03-05 00:23:18 +0100

    Add files via upload

--- bloker.ps1
+++ bloker.ps1
@@ -1,3 +1,4 @@
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
 param (
   [Parameter()]
   [switch]
@@ -7,7 +8,7 @@ param (
   $UpdateSpotify,
   [Parameter()]
   [switch]
-  $RemoveAdPlaceholder = (Read-Host -Prompt 'Opcjonalnie – Usunac symbol zastepczy reklamy i przycisk uaktualnienia. (Y/N)') -eq 'y'
+  $RemoveAdPlaceholder = (Read-Host -Prompt 'Opcjonalnie: Usun¹c symbol zastepczy reklamy i przycisk uaktualnienia. (Y/N)') -eq 'y'
 )
 
 # Ignore errors from `Stop-Process`
@@ -356,7 +357,6 @@ Write-Host 'Instalacja Zakonczona sukcesem.'
 
 Write-Host @'
 *****************
-@mrpond message:
 STOP WOJNIE
 *****************
 '@
